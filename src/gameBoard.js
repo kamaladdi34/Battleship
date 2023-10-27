da2664aeebfaf1aa76f43c72414a9b1d8167f7e9
Addi Kamal <kamal.addi@proton.me>
2023-10-27 14:00:42 +0100

    Add more tests to placeShip() method

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -6,8 +6,26 @@ class GameBoard {
     this.generateBoard(size);
   }
   placeShip(coordinates, shipLength) {
-    let newShip = new Ship(shipLength);
-    this.#board[coordinates.x][coordinates.y] = newShip;
+    let { x, y } = coordinates;
+    return new Promise((resolve, reject) => {
+      if (this.#coordinatesInRange(coordinates)) {
+        reject("Coordinates out of range");
+        return;
+      }
+      if (this.#board[x][y] !== "") {
+        reject("Target coordinates not empty");
+        return;
+      }
+      let newShip = new Ship(shipLength);
+      for (let i = 0; i < shipLength; i++) {
+        this.#board[x][y + i] = newShip;
+      }
+      resolve(newShip);
+    });
+  }
+  #coordinatesInRange(coordinates) {
+    let { x, y } = coordinates;
+    return x < 0 || y < 0 || x > this.#board.length || y > this.#board.length;
   }
   receiveAttack() {}
   allShipsAreSunk() {}
--- test/gameBoard.test.js
+++ test/gameBoard.test.js
@@ -29,4 +29,43 @@ describe("Tests for GameBoard placeShip() method", () => {
     let ship = new Ship(1);
     expect(gameBoard.getCell({ x: 0, y: 0 })).toEqual(ship);
   });
+
+  test("GameBoard places ship with length of 1 at [1,1]", () => {
+    let gameBoard = new GameBoard(10);
+    gameBoard.placeShip({ x: 1, y: 1 }, 1);
+    let ship = new Ship(1);
+    expect(gameBoard.getCell({ x: 1, y: 1 })).toEqual(ship);
+  });
+
+  test("GameBoard places doesn't place a ship when target is not empty", () => {
+    let gameBoard = new GameBoard(10);
+    gameBoard.placeShip({ x: 0, y: 0 }, 1);
+    gameBoard.placeShip({ x: 0, y: 0 }, 1).catch((error) => {
+      expect(error).toBe("Target coordinates not empty");
+    });
+  });
+
+  test("GameBoard returns error when providing incorrect coordinates", () => {
+    let gameBoard = new GameBoard(10);
+    expect(gameBoard.placeShip({ x: 22, y: 22 }, 1)).rejects.toMatch(
+      "Coordinates out of range"
+    );
+  });
+
+  test("GameBoard places ship with length of 2 at [0,0]", () => {
+    let gameBoard = new GameBoard(10);
+    gameBoard.placeShip({ x: 0, y: 0 }, 2);
+    let ship = new Ship(2);
+    expect(gameBoard.getCell({ x: 0, y: 0 })).toEqual(ship);
+    expect(gameBoard.getCell({ x: 0, y: 1 })).toEqual(ship);
+  });
+
+  test("GameBoard places ship with length of 3 at [0,0]", () => {
+    let gameBoard = new GameBoard(10);
+    gameBoard.placeShip({ x: 0, y: 0 }, 3);
+    let ship = new Ship(3);
+    expect(gameBoard.getCell({ x: 0, y: 0 })).toEqual(ship);
+    expect(gameBoard.getCell({ x: 0, y: 1 })).toEqual(ship);
+    expect(gameBoard.getCell({ x: 0, y: 2 })).toEqual(ship);
+  });
 });
