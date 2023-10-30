f67b9e54ff8be9df488ec3b45f79d00460e20ff3
Addi Kamal <kamal.addi@proton.me>
2023-10-30 11:00:27 +0100

    Initiate gameBoards in gameManager

--- src/gameManager.js
+++ src/gameManager.js
@@ -19,6 +19,18 @@ class GameManager {
       this.otherPlayer = new Player(players[1].name, players[1].isComputer);
       this.playerBoard = new GameBoard(boardSize);
       this.otherBoard = new GameBoard(boardSize);
+
+      this.playerBoard.placeShip({ x: 0, y: 0 }, 5);
+      this.playerBoard.placeShip({ x: 1, y: 0 }, 4);
+      this.playerBoard.placeShip({ x: 2, y: 0 }, 3);
+      this.playerBoard.placeShip({ x: 3, y: 0 }, 3);
+      this.playerBoard.placeShip({ x: 4, y: 0 }, 2);
+
+      this.otherBoard.placeShip({ x: 0, y: 0 }, 5);
+      this.otherBoard.placeShip({ x: 1, y: 0 }, 4);
+      this.otherBoard.placeShip({ x: 2, y: 0 }, 3);
+      this.otherBoard.placeShip({ x: 3, y: 0 }, 3);
+      this.otherBoard.placeShip({ x: 4, y: 0 }, 2);
       resolve("Game initiated");
     });
   }
